b6d300846b0631b22d16739ae073120bd32ed4c7
Abdul Hameed <raza2022@gmail.com>
2017-02-18 16:00:38 +0500

    code commented

--- polls/views.py
+++ polls/views.py
@@ -13,19 +13,19 @@ def index(request):
     # request.send_header('Access-Control-Allow-Origin', '*')
     # get the image by name can be changed
     base64_image_str = request.POST['croppedImage']
-
+    print(base64)
     # change base64 to image string else that line do nothing
     base64_image_str = base64_image_str[base64_image_str.find(",") + 1:]
 
     # decode string
-    imageData = base64.b64decode(base64_image_str)
+    # imageData = base64.b64decode(base64_image_str)
 
     # give unique file name and save in DB
-    dt = time.time()
-    filename = str(dt) + '.jpg'
-    print (filename)
-    with open(filename, 'wb') as f:
-        f.write(imageData)
+    # dt = time.time()
+    # filename = str(dt) + '.jpg'
+    # print (filename)
+    # with open(filename, 'wb') as f:
+    #     f.write(imageData)
 
     #     return response
     return HttpResponse("Hello, world. You're at the polls index.")
