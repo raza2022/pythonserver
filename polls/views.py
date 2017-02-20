fe096db95265217b020df206c05f940bd531eb2c
Abdul Hameed <raza2022@gmail.com>
2017-02-20 20:33:00 +0500

    match function added

--- polls/views.py
+++ polls/views.py
@@ -1,13 +1,13 @@
-from django.shortcuts import render
-
-# Create your views here.
 from django.http import HttpResponse
 from django.views.decorators.csrf import csrf_exempt
 import base64
 import time
-import datetime
-
 count = 1
+import tensorflow as tf, sys
+
+from django.shortcuts import render
+
+# Create your views here.
 @csrf_exempt
 def index(request):
     # request.send_header('Access-Control-Allow-Origin', '*')
@@ -31,9 +31,48 @@ def index(request):
     # give unique file name and save in DB
         dt = time.time()
         filename = str(dt) + '.jpg'
-        print(filename)
         with open(filename, 'wb') as f:
             f.write(imgdata)
+
+
+    # ################################################
+        image_path = filename
+        # Read in the image_data
+        image_data = tf.gfile.FastGFile(image_path, 'rb').read()
+
+        # Loads label file, strips off carriage return
+        label_lines = [line.rstrip() for line
+                    in tf.gfile.GFile("output_labels.txt")]
+
+        # Unpersists graph from file
+        with tf.gfile.FastGFile("output_graph.pb", 'rb') as f:
+            graph_def = tf.GraphDef()
+            graph_def.ParseFromString(f.read())
+            _ = tf.import_graph_def(graph_def, name='')
+
+        with tf.Session() as sess:
+        # Feed the image_data as input to the graph and get first prediction
+            softmax_tensor = sess.graph.get_tensor_by_name('final_result:0')
+
+            predictions = sess.run(softmax_tensor, {'DecodeJpeg/contents:0': image_data})
+
+        # Sort to show labels of first prediction in order of confidence
+            top_k = predictions[0].argsort()[-len(predictions[0]):][::-1]
+            print(top_k)
+        obj_list = []
+        for node_id in top_k:
+            human_string = label_lines[node_id]
+            score = predictions[0][node_id]
+            obj_list.append({human_string: score})
+            # print('%s (score = %.5f)' % (human_string, score))
+
+        return HttpResponse(obj_list)
+
+
+
+
+    # ################################################
+
 
     #     return response
     # return HttpResponse("Hello, world. You're at the polls index.")
