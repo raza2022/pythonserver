050c87a5e05c0245d8152f1a6ba020c7a6a67b3e
Abdul Hameed <raza2022@gmail.com>
2017-02-22 16:16:24 +0500

    debug == false

--- demo2/settings.py
+++ demo2/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'zd-+0#=r+2nd-47^-^up_njsn&7fg*1k_cpfp222c5b&sz(do0'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = [
 '127.0.0.1',
